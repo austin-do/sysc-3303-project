e3df1b88798bd8e1a47ef5aa7dae5a363c8437af
Victor <olaitanvictor99@gmail.com>
2020-01-30 20:39:56 -0500

    Almost done Elevator

--- src/Elevator.java
+++ src/Elevator.java
@@ -21,94 +21,108 @@ public class Elevator {
 		return poweredOn;
 	}
 
-	public void togglePowerdOn() {
-		poweredOn = !poweredOn;
+	public void powerOn() {
+		Thread taskGetter = new Thread(new TaskGetter());
+		poweredOn = true;
+		taskGetter.start();
 	}
 
-	public synchronized void tick() {
-		while (poweredOn) {
-
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-			}
-		}
+	public void powerOff() {
+		poweredOn = false;
 	}
 
 	// ========= Zeen's work ================
 
-	public synchronized void travel(int[][] tq) {
-
-		System.out.println("I'm Moving from " + tq[currentTask][0] + " floor!");
-
-		try {
-			Thread.sleep(tq[currentTask][2]);
-		} catch (InterruptedException e) {
-		}
-
-		System.out.println(tq[currentTask][3] + " floor Reached!");
-
-		// Send message to scheduler saying the elevator arrived destination?
-
-		// Open door and close door
-		toggleDoor(tq);
-
-	}
-
-	public synchronized void toggleDoor(int[][] tq) {
-
-		System.out.println("Door Opening");
-
-		try {
-			Thread.sleep(doorOpenTime);
-		} catch (InterruptedException e) {
-		}
-
-		System.out.println("Door Closing");
-
-		try {
-			Thread.sleep(doorCloseTime);
-		} catch (InterruptedException e) {
-		}
-
-		// Check queue. Another task? Next object in queue?
-		if (tq[++currentTask] == null) {
-			System.out.println("Elevator Wait for work");
-			thereIsWork = false;
-			getWork();
-		}
-
-		else {
-			travel(tq);
-		}
-
-	}
+//	public synchronized void travel(int[][] tq) {
+//
+//		System.out.println("I'm Moving from " + tq[currentTask][0] + " floor!");
+//
+//		try {
+//			Thread.sleep(tq[currentTask][2]);
+//		} catch (InterruptedException e) {
+//		}
+//
+//		System.out.println(tq[currentTask][3] + " floor Reached!");
+//
+//		// Send message to scheduler saying the elevator arrived destination?
+//
+//		// Open door and close door
+//		toggleDoor(tq);
+//
+//	}
+//
+//	public synchronized void toggleDoor(int[][] tq) {
+//
+//		System.out.println("Door Opening");
+//
+//		try {
+//			Thread.sleep(doorOpenTime);
+//		} catch (InterruptedException e) {
+//		}
+//
+//		System.out.println("Door Closing");
+//
+//		try {
+//			Thread.sleep(doorCloseTime);
+//		} catch (InterruptedException e) {
+//		}
+//
+//		// Check queue. Another task? Next object in queue?
+//		if (tq[++currentTask] == null) {
+//			System.out.println("Elevator Wait for work");
+//			thereIsWork = false;
+//			getWork();
+//		}
+//
+//		else {
+//			travel(tq);
+//		}
+//
+//	}
+//
+	// ======== Victor's work ===================
 
 	public static void main(String arg[]) {
-		System.out.println("Hello");
-		Elevator e = new Elevator();
-		e.getWork();
+		System.out.println("=== Testing Elevator Class ===");
+		Elevator elevator = new Elevator();
+		elevator.powerOn();
 	}
 
-	// ======== Victor's work ===================
+	private class TaskGetter implements Runnable {
+
+		@Override
+		public void run() {
+			System.out.println("\nAssigning a task");
+			state.assignTask(new Task(new Date(), 5, 17));
+			System.out.println("\nAssigning a task");
+			state.assignTask(new Task(new Date(), 1, 3));
+			System.out.println("\nAssigning a task");
+			state.assignTask(new Task(new Date(), 4, 20));
+			System.out.println("\nAssigning a task");
+			state.assignTask(new Task(new Date(), 13, 3));
+			System.out.println("\nAssigning a task");
+			state.assignTask(new Task(new Date(), 0, 1));
+		}
+
+	}
 
 	// only need to give Scheduler the ElevatorState to check if the elevator
 	// can stop at a floor. This protects data in Elevator
 	public class ElevatorState {
-		private final float ACCELERATION = 0.68f;
-		private final float MAX_VELOCITY = 4.31f;
-		private final float FLOOR_HEIGHT = 3.23f; // This should be a function call to Floor but whatever for now
+		private static final float ACCELERATION = 0.68f;
+		private static final float TERMINAL_VELOCITY = 4.31f;
+		private static final float FLOOR_HEIGHT = 3.23f; // This should be a function call to Floor but whatever for now
 
-		private Motion motion;
+		private ElevatorMotion motion;
 		private int currentFloor;
 		private float velocity;
 		private int direction; // 1 is up, -1 is down
 		private float metresTravelled; // per floor, NOT total
 
 		private ElevatorState() {
-			motion = new Motion();
+			motion = new ElevatorMotion();
 			currentFloor = 1;
-			velocity = 0;
+			velocity = ACCELERATION; // i can't prove it but velocity must be >= ACCELERATION
 			direction = 1;
 			metresTravelled = 0;
 		}
@@ -120,7 +134,7 @@ public class Elevator {
 		private void accelerate() {
 			if (!isMoving())
 				return;
-			doMovement(velocity = Math.min(velocity + ACCELERATION, MAX_VELOCITY));
+			doMovement(velocity = Math.min(velocity + ACCELERATION, TERMINAL_VELOCITY));
 		}
 
 		private void decelerate() {
@@ -135,6 +149,7 @@ public class Elevator {
 			if (metresTravelled >= FLOOR_HEIGHT) {
 				currentFloor += direction;
 				metresTravelled = 0;
+				System.out.print("|");
 			}
 		}
 
@@ -143,7 +158,8 @@ public class Elevator {
 		}
 
 		public boolean canStopAtFloor(int floor) {
-			if (taskQueue.isEmpty()) return true;
+			if (taskQueue.isEmpty())
+				return true;
 			if ((state.direction == -1 && floor > state.currentFloor)
 					|| (state.direction == 1 && floor < state.currentFloor)) {
 				return false;
@@ -152,59 +168,106 @@ public class Elevator {
 				return true;
 
 			float distanceToFloor = Math.abs(floor - state.currentFloor) * FLOOR_HEIGHT;
-			float secondsToFloor = distanceToFloor / state.velocity;
+			float secondsToFloor = distanceToFloor == 0 ? 0 : distanceToFloor / state.velocity;
 			return secondsToFloor >= state.secondsToStop();
 		}
 
-		public void assignTask(Task task) {
-			taskQueue.add(task);
+		public synchronized void assignTask(Task task) {
+			if (canStopAtFloor(task.startFloor)) {
+				System.out.println("Accepted task (" + task.startFloor + " -> " + task.endFloor + ")");
+
+				taskQueue.add(task);
+
+				if (!isAlive())
+					wakeup();
+			} else {
+				System.out.println("Cannot accept task (" + task.startFloor + " -> " + task.endFloor + ")");
+			}
 		}
 
 		private boolean isAlive() {
 			return motion.running;
 		}
 
-		private void wakeup() {
+		private synchronized void wakeup() {
+			if (isAlive())
+				return;
+			
 			Thread motionThread = new Thread(motion, "ElevatorMotion");
-			motionThread.run();
+			System.out.println("Waking up elevator");
+			motion.running = true;
+			motionThread.start();
 		}
 
-		private class Motion implements Runnable {
+		private final class ElevatorMotion implements Runnable {
 			private boolean running;
+			private boolean taskAssigned;
 
 			@Override
 			public void run() {
-				while (!taskQueue.isEmpty()) {
+				velocity = ACCELERATION;
+				metresTravelled = 0;
+				while (poweredOn && !taskQueue.isEmpty()) {
+					if (!taskAssigned) {
+						System.out.println("Starting a task");
+					}
+
 					Task currentTask = taskQueue.peek();
-					
-					int nextFloor = (currentTask.pickedUp ? currentTask.endFloor : currentTask.startFloor);
-					float distanceToFloor = Math.abs(
-							nextFloor - state.currentFloor)
-							* FLOOR_HEIGHT;
-					float secondsToFloor = distanceToFloor / state.velocity;
-					
+					direction = currentTask.nextFloor > currentFloor ? 1 : -1;
+
+					taskAssigned = true;
+
+					float distanceToFloor = Math.abs(currentTask.nextFloor - state.currentFloor) * FLOOR_HEIGHT;
+					float secondsToFloor = distanceToFloor == 0 ? 0 : distanceToFloor / state.velocity;
+
+//					System.out.print(nextFloor + ", " + distanceToFloor + ", " + secondsToFloor + " {} ");
+
 					if (secondsToFloor - 1 < state.secondsToStop()) {
-						decelerate();
-						if (state.currentFloor == nextFloor) {
-							velocity = 0;
+						if (state.currentFloor == currentTask.nextFloor) {
+							System.out.println("\nArrived at floor " + currentTask.nextFloor);
+
+							velocity = ACCELERATION;
 							metresTravelled = 0;
-							
+
 							doors.openDoors();
-							//TODO allow people to press button just in time and open doors again
+							// TODO allow people to press button just in time and open doors again
 							doors.closeDoors();
-							if (!currentTask.pickedUp) {
-								currentTask.pickedUp = true;
-							} else {
+							
+							if (currentTask.nextFloor == currentTask.endFloor) {
+								System.out.println("Completed task");
+								taskAssigned = false;
 								taskQueue.poll();
+							} else {
+								currentTask.nextFloor = currentTask.endFloor;
+								taskQueue.add(taskQueue.poll());
+								System.out.println("Moving towards floor " + taskQueue.peek().nextFloor);
 							}
+						} else {
+							if (velocity == TERMINAL_VELOCITY) {
+								System.out.print(".");// + currentFloor
+							} else {
+								System.out.print("-");
+							}
+
+							decelerate();
 						}
 					} else {
+						if (velocity == TERMINAL_VELOCITY) {
+							System.out.print(".");
+						} else {
+							System.out.print("+");
+						}
+
 						accelerate();
 					}
+
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+					}
 				}
-				velocity = 0;
-				metresTravelled = 0;
 				running = false;
+				System.out.println("\nElevator is sleeping");
 			}
 		}
 	}
@@ -218,6 +281,7 @@ public class Elevator {
 		private void openDoors() {
 			if (doorsOpen)
 				return;
+			System.out.println("Opening doors");
 			movingDirection = 1;
 			try {
 				Thread.sleep((long) (DOOR_MOVE_TIME * 1000));
@@ -229,6 +293,7 @@ public class Elevator {
 		private void closeDoors() {
 			if (!doorsOpen)
 				return;
+			System.out.println("Closing doors");
 			movingDirection = -1;
 			try {
 				Thread.sleep((long) (DOOR_MOVE_TIME * 1000));
@@ -239,38 +304,43 @@ public class Elevator {
 	}
 
 	// This is a model of the Scheduler class, not the real thing
-	public class Scheduler {
-		private final Task test1 = new Task(new Date(), state.currentFloor, state.currentFloor + 2);
-		private final Task test2 = new Task(new Date(), state.currentFloor + 4, state.currentFloor + 6);
-
-		private ElevatorState[] elevatorStates = new ElevatorState[1];
-
-		public void onReadTaskFromFile() {
-			Task task = new Random().nextBoolean() ? test1 : test2;
-			if (elevatorStates[0].canStopAtFloor(task.startFloor)) {
-				elevatorStates[0].assignTask(task);
-			} else {
-				// check other elevators. if none can do it, wait
-			}
-		}
-	}
+//	public static class Scheduler {
+//		private final Task test1 = ;
+//		private final Task test2 = ;
+//
+//		private ElevatorState[] elevatorStates = new ElevatorState[1];
+//
+//		public void onReadTaskFromFile() {
+//			Task task = new Random().nextBoolean() ? test1 : test2;
+//			if (elevatorStates[0].canStopAtFloor(task.startFloor)) {
+//				elevatorStates[0].assignTask(task);
+//			} else {
+//				// check other elevators. if none can do it, wait
+//			}
+//		}
+//	}
 
 	// This is a model of the Task class, not the real thing
 	public class Task implements Comparable<Task> {
 		private Date timeOfRequest;
 		private int startFloor;
 		private int endFloor;
-		private boolean pickedUp;
+		private int nextFloor;
 
 		public Task(Date timeOfRequest, int startFloor, int endFloor) {
 			this.timeOfRequest = timeOfRequest;
 			this.startFloor = startFloor;
 			this.endFloor = endFloor;
+			this.nextFloor = startFloor;
+		}
+		
+		public boolean pickedUp() {
+			return nextFloor == endFloor;
 		}
 
 		@Override
 		public int compareTo(Task o) {
-			return this.startFloor - o.startFloor;
+			return this.nextFloor - o.nextFloor;
 		}
 	}
 
